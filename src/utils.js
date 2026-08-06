3d1f0bea6581e28ddc27397876e0d5877b846e84
yoobonsu <124558193+yoobonsu@users.noreply.github.com>
2026-08-07 03:50:17 +0900

    feat: add sum

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,5 @@ exports.unique = (a) => [...new Set(a)];
 exports.clamp = (v, lo, hi) => Math.max(lo, Math.min(v, hi));
 
 exports.chunk = (a, n) => Array.from({ length: Math.ceil(a.length / n) }, (_, i) => a.slice(i * n, i * n + n));
+
+exports.sum = (a) => a.reduce((x, y) => x + y, 0);
