cc74419c10440decb6b3001a610b40623610c360
yoobonsu <124558193+yoobonsu@users.noreply.github.com>
2026-08-07 03:50:08 +0900

    feat: add chunk

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,5 @@ exports.reverse = (s) => String(s).split('').reverse().join('');
 exports.unique = (a) => [...new Set(a)];
 
 exports.clamp = (v, lo, hi) => Math.max(lo, Math.min(v, hi));
+
+exports.chunk = (a, n) => Array.from({ length: Math.ceil(a.length / n) }, (_, i) => a.slice(i * n, i * n + n));
